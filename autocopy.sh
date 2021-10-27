649d9d3e61ebdcad39d494e6992d0a1491628af3
Daniel Jones <daniel@danieljosephjones.com>
2021-10-27 11:56:23 -0400

    Revert "added autocopy script"
    This reverts commit a0b36ff0338cc5c4f6ed8e5813522adc4c8d333e.

--- autocopy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp -r ~/.config/alacritty/ ~/git/bspwmconfig/
-cp -r ~/.config/sxhkd/ ~/git/bspwmconfig/
-cp -r ~/.config/polybar/ ~/git/bspwmconfig/
-cp -r ~/.config/bspwm/ ~/git/bspwmconfig/
