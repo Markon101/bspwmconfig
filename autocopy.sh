62e134e32f700eacd6147607804a9632b16bd6c0
Daniel Jones <daniel@danieljosephjones.com>
2021-10-28 15:56:12 -0400

    added back autocopy script

---
+++ autocopy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp -r ~/.config/alacritty/ ~/git/bspwmconfig/
+cp -r ~/.config/sxhkd/ ~/git/bspwmconfig/
+cp -r ~/.config/polybar/ ~/git/bspwmconfig/
+cp -r ~/.config/bspwm/ ~/git/bspwmconfig/
